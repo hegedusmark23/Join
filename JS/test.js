b573d82d26f66683a83e4261ea14c9733ca2ea67
Christian Ment <christian.paul.ment@gmail.com>
2024-01-13 17:40:27 +0100

    add classes board and task

---
+++ JS/test.js
@@ -0,0 +1,11 @@
+let task = new Task(
+    1,
+    'test',
+    'lorem Ipsum dol et sieamet',
+    'Christian Ment',
+    '21-01-2024',
+    'medium',
+    '13-01-2024',
+    'ghdfgjhdfghh32424238432jnjnfj')
+
+console.log(task)
