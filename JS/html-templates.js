b78a0ac0210bb993f11a5a3acd19f7d706b985be
Mark Hegedus <hegeee23@hotmail.comgit config --global user.email hegeee23@hotmail.com>
2024-01-14 16:51:10 +0100

    work on summary page

--- JS/html-templates.js
+++ JS/html-templates.js
@@ -1,5 +1,5 @@
 
-function summaryHTML(){
+function summaryHTML() {
     return /*html*/`
         <div class="summary-container">
             <div class="summary-headline">
@@ -7,13 +7,45 @@ function summaryHTML(){
                 <div class="vert-blue-line"></div>
                 <p class="summary-headline-normal">Key Metrics at a Glance</p>
             </div>
-            <div>
-                <div>
+            <div class="summary-main-content">
+                <div class="tasks-summary-section">
 
+                    <div class="summary-task-display-mid">
+                        <div class="summary-icon-circle"><img src="/assets/img/pencil-white.png" alt=""></div>
+                        <div>
+                            <h1 class="tasks-number">0</h1>
+                            <span class="summary-text">To-do</span>
+                        </div>
+                    </div>
+
+                    <div class="summary-task-display-mid">
+                        <div class="summary-icon-circle"><img src="/assets/img/check-white.png" alt=""></div>
+                        <div>
+                            <h1 class="tasks-number">0</h1>
+                            <span class="summary-text">Done</span>
+                        </div>
+                    </div>
+
+                    <div class="summary-task-display-long">
+                        <div>
+                            <div><img src="/assets/img/double-arrows-up.png" alt=""></div>
+                            <div>
+                            <h1 class="tasks-number">0</h1>
+                            <span class="summary-text">Urgent</span>
+                            </div>
+                        </div>
+                        <div class="gray-vert-line"></div>
+                        <div>
+                            <h3>Sample date</h3>
+                            <span>Upcoming Deadline</span>
+                        </div>
+                    </div>
+                    <div></div>
+                    <div></div>
                 </div>
                 <div class="welcome-section">
-                    <h1>Sample Daytime,</h1>
-                    <h1>Sample User</h1>
+                    <h1 class="daytime-text">Sample Daytime,</h1><br>
+                    <h1 class="user-name-text">Sample User</h1>
                 </div>
             </div>
         </div>
