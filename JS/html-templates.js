3c196d0ba7c2c5fca14e98a0086525b716921e40
Mark Hegedus <hegeee23@hotmail.comgit config --global user.email hegeee23@hotmail.com>
2024-01-16 17:43:56 +0100

    unnecessary html files deleted now its loaded by JS to the main content

--- JS/html-templates.js
+++ JS/html-templates.js
@@ -65,4 +65,217 @@ function summaryHTML() {
     `;
 }
 
+function legalNoticeHTML(){
+    return /*html*/`
+        <a class="legal-site-arrow" onclick="generateSummaryContent()"><img class="arrow" src="/assets/img/arrow-left-blue.png" alt=""></a>
+            <div id="summary-content">
+                <div class="legal-text-container">
+                    <div>
+                        <h1 class="legal-headline">Privacy Policy</h1>
+                        
+                    </div>
+                    <div>
+                        <h2>Subtitle</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa
+                        eget, ornare lacinia urna. In dignissim justo eu velit sagittis, in scelerisque nulla convallis.
+                        Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris. Sed turpis ipsum,
+                        condimentum quis nulla at, lobortis facilisis ipsum. Nunc erat justo, hendrerit vel enim vitae,
+                        feugiat mattis dui. In auctor dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent
+                        tempus congue magna. Quisque libero erat, pharetra a neque et, imperdiet semper justo.</p>
+                        <h2>Subtitle</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa
+                        eget, ornare lacinia urna. In dignissim justo eu velit sagittis, in scelerisque nulla convallis.
+                        Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris. Sed turpis ipsum,
+                        condimentum quis nulla at, lobortis facilisis ipsum. Nunc erat justo, hendrerit vel enim vitae,
+                        feugiat mattis dui. In auctor dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent
+                        tempus congue magna. Quisque libero erat, pharetra a neque et, imperdiet semper justo.</p>
+    
+                    </div>
+                </div>
+            </div>
+`;
+}
 
+function privacyPolicyHTML(){
+    return /*html*/`
+        <a class="legal-site-arrow" onclick="generateSummaryContent()"><img class="arrow"
+                    src="/assets/img/arrow-left-blue.png" alt=""></a>
+            <div id="summary-content">
+                <div class="legal-text-container">
+                    <div>
+                        <h1 class="legal-headline">Legal Notice</h1>
+
+                    </div>
+                    <div>
+                        <h2>Imprint</h2>
+                        <li>[Student Names List]</li>
+                        <li>[Address of the JOIN operator - e.g. one of the students]</li>
+                        <li>[Postcode and city]</li>
+                        <h2>Exploring the Board</h2>
+                        Email: [Email]
+                        <h2>Acceptance of terms</h2>
+                        <p>By accessing and using <span class="blue-text">Join</span> (Product), you acknowledge and
+                            agree to the following terms and
+                            conditions, and any policies, guidelines, or amendments thereto that may be presented to you
+                            from time to time. We, the listed students, may update or change the terms and conditions
+                            from time to time without notice.</p>
+                        <h2>Scope and ownership of the product</h2>
+                        <p><span class="blue-text">Join</span> has been developed as part of a student group project in
+                            a web development bootcamp at the
+                            <span class="blue-text">Developer Akademie GmbH</span>. It has an educational purpose and is
+                            not intended for extensive
+                            personal & business usage. As such, we cannot guarantee consistent availability,
+                            reliability,
+                            accuracy, or any other aspect of quality regarding this Product.
+                        </p>
+                        <br>
+                        <p>The design of <span class="blue-text">Join</span> is owned by the <span
+                                class="blue-text">Developer Akademie GmbH</span>. Unauthorized use, reproduction,
+                            modification, distribution, or replication of the design is strictly prohibited.</p>
+                        <h2>Proprietary rights</h2>
+                        <p>Aside from the design owned by <span class="blue-text">Developer Akademie GmbH</span>, we,
+                            the listed students, retain all
+                            proprietary rights in <span class="blue-text">Join</span>, including any associated
+                            copyrighted material, trademarks, and
+                            other
+                            proprietary information.</p>
+                        <h2>Use of the product</h2>
+                        <p><span class="blue-text">Join</span> is intended to be used for lawful purposes only, in
+                            accordance with all applicable laws
+                            and
+                            regulations. Any use of <span class="blue-text">Join</span> for illegal activities, or to
+                            harass, harm, threaten, or
+                            intimidate
+                            another person, is strictly prohibited. You are solely responsible for your interactions
+                            with
+                            other users of <span class="blue-text">Join</span>.</p>
+                        <h2>Disclaimer of warranties and limitation of liability</h2>
+                        <p><span class="blue-text">Join</span> is provided "as is" without warranty of any kind, whether
+                            express or implied, including
+                            but
+                            not limited to the implied warranties of merchantability, fitness for a particular purpose,
+                            and
+                            non-infringement. In no event will we, the listed students, or the <span
+                                class="blue-text">Developer Akademie</span>, be
+                            liable
+                            for any direct, indirect, incidental, special, consequential or exemplary damages, including
+                            but
+                            not limited to, damages for loss of profits, goodwill, use, data, or other intangible
+                            losses,
+                            even if we have been advised of the possibility of such damages, arising out of or in
+                            connection
+                            with the use or performance of <span class="blue-text">Join</span>.</p>
+                        <h2>Indemnity</h2>
+                        <p>You agree to indemnify, defend and hold harmless us, the listed students, the <span
+                                class="blue-text">Developer
+                                Akademie</span>, and our affiliates, partners, officers, directors, agents, and
+                            employees, from and
+                            against any claim, demand, loss, damage, cost, or liability (including reasonable legal
+                            fees)
+                            arising out of or relating to your use of <span class="blue-text">Join</span> and/or your
+                            breach of this Legal Notice.</p>
+                        <br>
+                        <p>For any questions or notices, please contact us at [Contact Email].</p>
+                        <br>
+                        <p>Date: July 26, 2023</p>
+                    </div>
+                </div>
+            </div>
+    `;
+}
+
+function helpHTML(){
+    return /*html*/`
+    <a class="legal-site-arrow" onclick="generateSummaryContent()"><img class="arrow"
+    src="/assets/img/arrow-left-blue.png" alt=""></a>
+<div id="summary-content">
+<div class="legal-text-container">
+    <div>
+        <h1 class="legal-headline">Help</h1>
+
+    </div>
+    <div>
+        Welcome to the help page for <span class="blue-text">Join</span>, your guide to using our kanban
+        project management tool. Here,
+        we'll provide an overview of what <span class="blue-text">Join</span> is, how it can benefit
+        you, and how to use it.
+        <h2>What is Join?</h2>
+        <p><span class="blue-text">Join</span> is a kanban-based project management tool designed and
+            built by a group of dedicated
+            students as part of their web development bootcamp at the Developer Akademie.</p>
+        <br>
+        <p>Kanban, a Japanese term meaning "billboard", is a highly effective method to visualize work,
+            limit work-in-progress, and maximize efficiency (or flow). <span
+                class="blue-text">Join</span> leverages the principles of
+            kanban to help users manage their tasks and projects in an intuitive, visual interface.</p>
+        <br>
+        <p>It is important to note that Join is designed as an educational exercise and is not intended
+            for
+            extensive business usage. While we strive to ensure the best possible user experience, we
+            cannot
+            guarantee consistent availability, reliability, accuracy, or other aspects of quality
+            regarding
+            <span class="blue-text">Join</span>.
+        </p>
+        <h2>How to use it</h2>
+        <p>Here is a step-by-step guide on how to use <span class="blue-text">Join</span>:</p>
+
+
+
+        <ol type="1">
+            <li>
+                <h3>Exploring the Board</h3>
+                When you log in to Join, you'll find a default board. This board represents your project
+                and
+                contains four default lists: "To Do", "In Progress", “Await feedback” and "Done".
+            </li>
+
+
+            <li>
+                <h3>Creating Contacts</h3>
+                In Join, you can add contacts to collaborate on your projects. Go to the "Contacts"
+                section,
+                click on "New contact", and fill in the required information. Once added, these contacts
+                can
+                be
+                assigned tasks and they can interact with the tasks on the board.
+            </li>
+
+            <li>
+                <h3>Adding Cards</h3>
+                Now that you've added your contacts, you can start adding cards. Cards represent
+                individual
+                tasks. Click the "+" button under the appropriate list to create a new card. Fill in the
+                task
+                details in the card, like task name, description, due date, assignees, etc.
+            </li>
+            <li>
+                <h3>Moving Cards</h3>
+                As the task moves from one stage to another, you can reflect that on the board by
+                dragging and
+                dropping the card from one list to another.
+            </li>
+            <li>
+                <h3>Deleting Cards</h3>
+                Once a task is completed, you can either move it to the "Done" list or delete it.
+                Deleting a
+                card will permanently remove it from the board. Please exercise caution when deleting
+                cards, as
+                this action is irreversible.
+            </li>
+        </ol>
+        <p>Remember that using <span class="blue-text">Join</span> effectively requires consistent
+            updates from you and your team to
+            ensure the board reflects the current state of your project.</p>
+        <br>
+        <p>Have more questions about <span class="blue-text">Join</span>? Feel free to contact us at
+            [Your Contact Email]. We're here to
+            help you!</p>
+
+        <h2>Enjoy using Join!</h2>
+
+    </div>
+</div>
+</div>
+    `;
+}
--- JS/main-content.js
+++ JS/main-content.js
@@ -1,29 +1,44 @@
 
-function generateSummaryContent(){              
-    let container = document.getElementById('main-content');
-    let background = document.getElementById('summary-nav');
-    container.innerHTML = summaryHTML();
-    background.classList.add('selected-nav-link-background');
+function generateSummaryContent() {
+  let container = document.getElementById('main-content');
+  let background = document.getElementById('summary-nav');
+  container.innerHTML = summaryHTML();
+  background.classList.add('selected-nav-link-background');
 }
 
-function changePencilImgColor(){                //Changes the color of an icon on hover
-    var image = document.getElementById("pencil-icon");
-    var button = document.getElementById("pencil-button");
-    button.addEventListener('mouseover', function(){
-      image.src = "/assets/img/pencil-blue.png"
-    })
-    button.addEventListener('mouseout', function(){
-        image.src = "/assets/img/pencil-white.png"
-      })
+function generateLegalNoticeContent() {
+  let container = document.getElementById('main-content');
+  container.innerHTML = legalNoticeHTML();
 }
 
-function changeCheckImgColor(){         //Changes the color of an icon on hover
-    var image = document.getElementById("check-icon");
-    var button = document.getElementById("check-button");
-    button.addEventListener('mouseover', function(){
-      image.src = "/assets/img/check-blue.png"
-    })
-    button.addEventListener('mouseout', function(){
-        image.src = "/assets/img/check-white.png"
-      })
+function generatePrivacyPolicyContent(){
+  let container = document.getElementById('main-content');
+  container.innerHTML = privacyPolicyHTML();
+}
+
+function generateHelpContent(){
+  let container = document.getElementById('main-content');
+  container.innerHTML = helpHTML();
+}
+
+function changePencilImgColor() {                //Changes the color of an icon on hover
+  var image = document.getElementById("pencil-icon");
+  var button = document.getElementById("pencil-button");
+  button.addEventListener('mouseover', function () {
+    image.src = "/assets/img/pencil-blue.png"
+  })
+  button.addEventListener('mouseout', function () {
+    image.src = "/assets/img/pencil-white.png"
+  })
+}
+
+function changeCheckImgColor() {         //Changes the color of an icon on hover
+  var image = document.getElementById("check-icon");
+  var button = document.getElementById("check-button");
+  button.addEventListener('mouseover', function () {
+    image.src = "/assets/img/check-blue.png"
+  })
+  button.addEventListener('mouseout', function () {
+    image.src = "/assets/img/check-white.png"
+  })
 }
