514e321a9d37e58a9dd0048ec7025c5b2532cd80
vincenzorispoli.vr@gmail.com <you@example.com>
2024-03-01 22:49:18 +0100

    new code board_dragdrop

--- JS/board_dragdrop.js
+++ JS/board_dragdrop.js
@@ -1,40 +1,40 @@
-let todos;
-let inProgress;
-let awaitFeedback;
-let done;
+
 /**
  * Startet den Drag-Vorgang für ein Task-Element.
  * @param {number} id - Die ID des zu verschiebenden Tasks.
  * @param {string} state - Der aktuelle Zustand des Tasks.
  * @param {number} i - Der Index des Tasks im aktuellen Zustand.
+*/
+/**
+ * Ermöglicht das Ablegen von Drag-Elementen.
+ * @param {Event} ev - Das Drag-Event.
  */
+function allowDrop(event) {
+    event.preventDefault();
+}
+
 function startDragging(id, state, i) {
     currentDraggedElement = id;
     document.getElementById(`${state}-card-content${i}`).style.rotate = '10deg'
 }
 
-/**
- * Ermöglicht das Ablegen von Drag-Elementen.
- * @param {Event} ev - Das Drag-Event.
- */
-function allowDrop(ev) {
-    ev.preventDefault();
-}
 
 /**
  * Verschiebt den aktuell gezogenen Task in einen neuen Zustand und aktualisiert die Ansicht.
  * @param {string} state - Der Zielzustand des Tasks.
- */
+*/
 async function moveTo(state) {
     tasks[currentDraggedElement]['state'] = state;
     await setItem('tasks', JSON.stringify(tasks));
     await initializeBoardCard();
 }
 
+
 /**
  * Versteckt den "Keine Tasks"-Hinweis für eine spezifische Spalte, wenn ein Task hinzugefügt wird.
  * @param {string} id - Die ID der Spalte, in der der Hinweis versteckt werden soll.
  */
+
 function hideNoTaskDiv(id) {
     let background;
     if (id == 'toDo') {
@@ -54,14 +54,17 @@ function hideNoTaskDiv(id) {
  * @param {string} id - Die ID der Spalte, für die der Hinweis zurückgesetzt werden soll.
  */
 function resetNoTaskDiv(id) {
+    let background;
     if (id == 'toDo' && todos.length == 0) {
         background = 'board-card-background-1';
     } else if (id == 'in-progress' && inProgress.length == 0) {
         background = 'board-card-background-2';
-    } else if (id == 'await-feedback' && awaitFeedback.length == 0) {
+    } else if (id == 'await-feedback' && awaitFeedback.length == 0 ) {
         background = 'board-card-background-3';
     } else if (id == 'done' && done.length == 0) {
         background = 'board-card-background-4'
     }
-    document.getElementById(background).style.display = ''
+    if(background){
+        document.getElementById(background).style.display = 'flex'
+    } else {}
 }
