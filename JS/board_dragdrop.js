06b266ed12c7c8c13f1a11f2e395d22778e5d592
vincenzorispoli.vr@gmail.com <you@example.com>
2024-03-08 22:29:08 +0100

    mobile dragNdrop completed

--- JS/board_dragdrop.js
+++ JS/board_dragdrop.js
@@ -145,30 +145,38 @@ function showPopUpMoveTaskMobile(id, event) {
     let mobileDialog = document.getElementById('dialog-container-card-to-move');
     mobileDialog.classList.remove('d-none');
     mobileDialog.innerHTML = /*html*/ `
-       <div onclick="doNotClose(event)" class="popUp-choice">
+       <div onclick="doNotClose(event)" id="popUp-choice" class="popUp-choice translatePopUpChoice">
         <span onclick="closePopUpMoveTaskMobile(event)" class="cross-icon-choice-container">
         <svg class="cross-icon-btn1" width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.001 12.5001L17.244 17.7431M6.758 17.7431L12.001 12.5001L6.758 17.7431ZM17.244 7.25708L12 12.5001L17.244 7.25708ZM12 12.5001L6.758 7.25708L12 12.5001Z" stroke="#2A3647" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
           </svg>
         </span>
            <h2 class="popUp-choice-title">Wohin möchten Sie dies Task verschieben?</h2>
-          <p id="toDo" class="choice" onclick="moveTo('toDo')">To Do</p>
-          <p id="in-progress" class="choice" onclick="moveTo('in-progress')">In Progress</p>
-          <p id="await-feedback" class="choice" onclick="moveTo('await-feedback')">Await Feedback</p>
-          <p id="done" class="choice" onclick="moveTo('done')">Done</p>
+          <p id="toDoM" class="choice" onclick="moveTo('toDo',event)">To Do</p>
+          <p id="in-progressM" class="choice" onclick="moveTo('in-progress',event)">In Progress</p>
+          <p id="await-feedbackM" class="choice" onclick="moveTo('await-feedback',event)">Await Feedback</p>
+          <p id="doneM" class="choice" onclick="moveTo('done',event)">Done</p>
       </div>
     `
+    setTimeout(() => {
+        document.getElementById('popUp-choice').classList.remove('translatePopUpChoice');
+    }, 100)
 }
 
-async function moveTo(state) {
+async function moveTo(state, event) {
+    let stateForId = state + 'M'
     tasks[currentDraggedElement]['state'] = state;
+    document.getElementById(stateForId).classList.add('blue-btn');
+    document.getElementById(stateForId).style.color = '#fff';
     await setItem('tasks', JSON.stringify(tasks));
     await initializeBoardCard();
-    document.getElementById('dialog-container-card-to-move').classList.add('d-none')
-
+    closePopUpMoveTaskMobile(event);
 }
 
 function closePopUpMoveTaskMobile(event) {
-    document.getElementById('dialog-container-card-to-move').classList.add('d-none');
+    document.getElementById('popUp-choice').classList.add('translatePopUpChoice');
+    setTimeout(() => {
+        document.getElementById('dialog-container-card-to-move').classList.add('d-none');
+    }, 200)
     event.stopPropagation();
 }
